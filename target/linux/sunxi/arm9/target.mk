c9c072a14504cc92774092d72256ab15bc0e8542
handsomeyingyan <handsomeyingyan@github.com>
2020-04-25 09:23:03 +0800

    fix sun4i-code build error

--- target/linux/sunxi/arm9/target.mk
+++ target/linux/sunxi/arm9/target.mk
@@ -1,5 +1,6 @@
 #
 # Copyright (C) 2017 Hauke Mehrtens
+# Copyright (C) 2020 Handsomemod Project
 #
 # This is free software, licensed under the GNU General Public License v2.
 # See /LICENSE for more information.
@@ -7,6 +8,5 @@
 
 include $(TOPDIR)/rules.mk
 
-BOARDNAME:=Allwinner F1CX00s
+BOARDNAME:=Allwinner F1C100s
 CPU_TYPE:=arm926ej-s
-

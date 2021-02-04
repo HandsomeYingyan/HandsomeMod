ad7ae6a4d03cc154699ad2e03155b43ae6eb619b
handsomeyingyan <handsomeyingyan@github.com>
2021-02-04 20:33:08 +0800

    [csi] add more camera model v2

--- package/kernel/linux/modules/video.mk
+++ package/kernel/linux/modules/video.mk
@@ -32,6 +32,7 @@ define KernelPackage/video-core
   	CONFIG_MEDIA_CONTROLLER_REQUEST_API=n \
   	CONFIG_VIDEO_TDA1997X=n \
   	CONFIG_VIDEO_ADV748X=n \
+  	CONFIG_VIDEO_ADV7604=n \
   	CONFIG_VIDEO_VIDEO_ADV7604=n \
   	CONFIG_VIDEO_MUX=n \
   	CONFIG_VIDEO_ADV7842=n \
@@ -43,6 +44,7 @@ define KernelPackage/video-core
 	CONFIG_VIDEO_CAPTURE_DRIVERS=y \
 	CONFIG_V4L_USB_DRIVERS=y \
 	CONFIG_V4L_PCI_DRIVERS=y \
+	CONFIG_V4L2_FWNODE=y \
 	CONFIG_V4L_PLATFORM_DRIVERS=y \
 	CONFIG_V4L_ISA_PARPORT_DRIVERS=y
   FILES:= \
@@ -695,7 +697,7 @@ endef
 $(eval $(call KernelPackage,video-ov5640))
 
 define KernelPackage/video-ov2680
-  TITLE:=OmniVision OV5640 sensor support
+  TITLE:=OmniVision OV2680 sensor support
   KCONFIG:=CONFIG_VIDEO_OV2680
   FILES:=$(LINUX_DIR)/drivers/media/i2c/ov2680.ko
   AUTOLOAD:=$(call AutoProbe,ov2680)
@@ -707,6 +709,188 @@ define KernelPackage/video-ov2680/description
 endef
 
 $(eval $(call KernelPackage,video-ov2680))
+
+define KernelPackage/video-ov2685
+  TITLE:=OmniVision ov2685 sensor support
+  KCONFIG:=CONFIG_VIDEO_OV2685
+  FILES:=$(LINUX_DIR)/drivers/media/i2c/ov2685.ko
+  AUTOLOAD:=$(call AutoProbe,ov2685)
+  $(call AddDepends/camera)
+endef
+
+define KernelPackage/video-ov2685/description
+ OmniVision ov2685 sensor support
+endef
+
+$(eval $(call KernelPackage,video-ov2685))
+
+define KernelPackage/video-ov5645
+  TITLE:=OmniVision ov5645 sensor support
+  KCONFIG:=CONFIG_VIDEO_OV5645
+  FILES:=$(LINUX_DIR)/drivers/media/i2c/ov5645.ko
+  AUTOLOAD:=$(call AutoProbe,ov5645)
+  $(call AddDepends/camera)
+endef
+
+define KernelPackage/video-ov5645/description
+ OmniVision ov5645 sensor support
+endef
+
+$(eval $(call KernelPackage,video-ov5645))
+
+define KernelPackage/video-ov5647
+  TITLE:=OmniVision ov5647 sensor support
+  KCONFIG:=CONFIG_VIDEO_OV5647
+  FILES:=$(LINUX_DIR)/drivers/media/i2c/ov5647.ko
+  AUTOLOAD:=$(call AutoProbe,ov5647)
+  $(call AddDepends/camera)
+endef
+
+define KernelPackage/video-ov5647/description
+ OmniVision ov5647 sensor support
+endef
+
+$(eval $(call KernelPackage,video-ov5647))
+
+define KernelPackage/video-ov5670
+  TITLE:=OmniVision ov5670 sensor support
+  KCONFIG:=CONFIG_VIDEO_OV5670
+  FILES:=$(LINUX_DIR)/drivers/media/i2c/ov5670.ko
+  AUTOLOAD:=$(call AutoProbe,ov5670)
+  $(call AddDepends/camera)
+endef
+
+define KernelPackage/video-ov5670/description
+ OmniVision ov5670 sensor support
+endef
+
+$(eval $(call KernelPackage,video-ov5670))
+
+define KernelPackage/video-ov5675
+  TITLE:=OmniVision ov5675 sensor support
+  KCONFIG:=CONFIG_VIDEO_OV5675
+  FILES:=$(LINUX_DIR)/drivers/media/i2c/ov5675.ko
+  AUTOLOAD:=$(call AutoProbe,ov5675)
+  $(call AddDepends/camera)
+endef
+
+define KernelPackage/video-ov5675/description
+ OmniVision ov5675 sensor support
+endef
+
+$(eval $(call KernelPackage,video-ov5675))
+
+define KernelPackage/video-ov7251
+  TITLE:=OmniVision ov7251 sensor support
+  KCONFIG:=CONFIG_VIDEO_OV7251
+  FILES:=$(LINUX_DIR)/drivers/media/i2c/ov7251.ko
+  AUTOLOAD:=$(call AutoProbe,ov7251)
+  $(call AddDepends/camera)
+endef
+
+define KernelPackage/video-ov7251/description
+ OmniVision ov7251 sensor support
+endef
+
+$(eval $(call KernelPackage,video-ov7251))
+
+define KernelPackage/video-ov8856
+  TITLE:=OmniVision ov8856 sensor support
+  KCONFIG:=CONFIG_VIDEO_OV8856
+  FILES:=$(LINUX_DIR)/drivers/media/i2c/ov8856.ko
+  AUTOLOAD:=$(call AutoProbe,ov8856)
+  $(call AddDepends/camera)
+endef
+
+define KernelPackage/video-ov8856/description
+ OmniVision ov8856 sensor support
+endef
+
+$(eval $(call KernelPackage,video-ov8856))
+
+define KernelPackage/video-ov9650
+  TITLE:=OmniVision ov9650 sensor support
+  KCONFIG:=CONFIG_VIDEO_OV9650
+  FILES:=$(LINUX_DIR)/drivers/media/i2c/ov9650.ko
+  AUTOLOAD:=$(call AutoProbe,ov9650)
+  $(call AddDepends/camera)
+endef
+
+define KernelPackage/video-ov9650/description
+ OmniVision ov9650 sensor support
+endef
+
+$(eval $(call KernelPackage,video-ov9650))
+
+define KernelPackage/video-ov13858
+  TITLE:=OmniVision ov13858 sensor support
+  KCONFIG:=CONFIG_VIDEO_OV13858
+  FILES:=$(LINUX_DIR)/drivers/media/i2c/ov13858.ko
+  AUTOLOAD:=$(call AutoProbe,ov13858)
+  $(call AddDepends/camera)
+endef
+
+define KernelPackage/video-ov13858/description
+ OmniVision ov13858 sensor support
+endef
+
+$(eval $(call KernelPackage,video-ov13858))
+
+define KernelPackage/video-mt9m001
+  TITLE:=mt9m001 sensor support
+  KCONFIG:=CONFIG_VIDEO_MT9M001
+  FILES:=$(LINUX_DIR)/drivers/media/i2c/mt9m001.ko
+  AUTOLOAD:=$(call AutoProbe,mt9m001)
+  $(call AddDepends/camera)
+endef
+
+define KernelPackage/video-mt9m001/description
+ mt9m001 sensor support
+endef
+
+$(eval $(call KernelPackage,video-mt9m001))
+
+define KernelPackage/video-mt9m032
+  TITLE:=mt9m032 sensor support
+  KCONFIG:=CONFIG_VIDEO_MT9M032
+  FILES:=$(LINUX_DIR)/drivers/media/i2c/mt9m032.ko
+  AUTOLOAD:=$(call AutoProbe,mt9m032)
+  $(call AddDepends/camera)
+endef
+
+define KernelPackage/video-mt9m032/description
+ mt9m032 sensor support
+endef
+
+$(eval $(call KernelPackage,video-mt9m032))
+
+define KernelPackage/video-mt9p031
+  TITLE:=MT9P031 sensor support
+  KCONFIG:=CONFIG_VIDEO_MT9M032
+  FILES:=$(LINUX_DIR)/drivers/media/i2c/mt9p031.ko
+  AUTOLOAD:=$(call AutoProbe,mt9p031)
+  $(call AddDepends/camera)
+endef
+
+define KernelPackage/video-mt9p031/description
+ mt9p031 sensor support
+endef
+
+$(eval $(call KernelPackage,video-mt9p031))
+
+define KernelPackage/video-mt9t001
+  TITLE:=MT9T001 sensor support
+  KCONFIG:=CONFIG_VIDEO_MT9T001
+  FILES:=$(LINUX_DIR)/drivers/media/i2c/mt9t001.ko
+  AUTOLOAD:=$(call AutoProbe,mt9t001)
+  $(call AddDepends/camera)
+endef
+
+define KernelPackage/video-mt9t001/description
+ mt9t001 sensor support
+endef
+
+$(eval $(call KernelPackage,video-mt9t001))
 
 define KernelPackage/video-imx214
   TITLE:=Sony imx214 sensor support
